2c424c17157af2ddf9c4e35c4250a57b828567f1
M. Taufik Nelas <fickysyahreza.fs@gmail.com>
2020-12-29 21:57:30 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 echo "===================================="
 echo "       ,~~.                         "
 echo "      (  9 )-_,                     "
-echo " (\___ )=='-'                       " 
+echo " (\___ )=='-'                       "
 echo " \ .   ) )                          "
-echo "  \ `-' /                           "
-echo "   `~j-'   SIMPLE-VNC               "
-echo "     "=:                            "
+echo "  \ '-' /                           "
+echo "   '~j-'   SIMPLE-VNC               "
+echo "     |=:                            "
 echo "===================================="
+apt-get update
 apt-get install -y xfonts-100dpi xfonts-100dpi-transcoded xfonts-75dpi xfonts-75dpi-transcoded xfonts-base
 apt-get install -y tightvncserver
 apt-get install -y xfonts-100dpi xfonts-100dpi-transcoded xfonts-75dpi xfonts-75dpi-transcoded xfonts-base
